24fa11a0bf82f65a58030c0efb2727c95364f894
maja <maja.dabcevic@gmail.com>
2023-06-17 00:22:13 +0200

    WIP - adding interface for IniSettings

---
+++ Helpers/IniSettingsHelper.h
@@ -0,0 +1,33 @@
+//---------------------------------------------------------------------------
+
+#ifndef IniSettingsHelperH
+#define IniSettingsHelperH
+#include <System.hpp> // Include for UnicodeString
+#include <Registry.hpp> //Include for TIniFile
+//---------------------------------------------------------------------------
+/*
+* @brief Class that provides all that is necessary for creating INI settings.
+*
+*
+*/
+ class IniSettingsHelper
+{
+public:
+	TIniFile *ini = new TIniFile("settings.ini");
+	UnicodeString path;
+	UnicodeString sectionName;
+
+	virtual void resetToDefault();
+	virtual void LoadSection(UnicodeString name);
+	virtual void EditSection(UnicodeString name);
+
+    virtual ~IniSettingsHelper()
+	{
+		delete ini;
+	}
+
+};
+//---------------------------------------------------------------------------
+extern PACKAGE IniSettingsHelper *IniSettingsHelper;
+//---------------------------------------------------------------------------
+#endif
