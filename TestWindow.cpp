b322a7246b937f56ed308a33342b0d54ff642c3d
maja <maja.dabcevic@gmail.com>
2023-06-25 13:40:51 +0200

    Testing - load INI settings

--- TestWindow.cpp
+++ TestWindow.cpp
@@ -4,6 +4,7 @@
 #pragma hdrstop
 
 #include "TestWindow.h"
+#include "Data/AllData.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
@@ -14,3 +15,18 @@ __fastcall TForm2::TForm2(TComponent* Owner)
 {
 }
 //---------------------------------------------------------------------------
+void __fastcall TForm2::LoadIni_testClick(TObject *Sender)
+{
+	DataModule1->themeHelper.LoadSection("CURRENT THEME");
+	ShowMessage(DataModule1->themeHelper.textColour);
+	ShowMessage(DataModule1->themeHelper.backgroundColour);
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm2::SaveIni_TestClick(TObject *Sender)
+{
+	DataModule1->fontHelper.LoadSection("CURRENT FONT");
+	ShowMessage(DataModule1->fontHelper.fontName);
+	ShowMessage(DataModule1->fontHelper.textHeight);
+}
+//---------------------------------------------------------------------------
+
