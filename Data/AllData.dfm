1b8238504a02988802e4cca57a7308429e6b9995
maja <maja.dabcevic@gmail.com>
2023-06-30 19:30:53 +0200

    Component - TXML, paired with companycars helper

--- Data/AllData.dfm
+++ Data/AllData.dfm
@@ -1,4 +1,11 @@
 object DataModule1: TDataModule1
   Height = 480
   Width = 640
+  object XmlDoc: TXMLDocument
+    FileName = 
+      'C:\Users\Maja\Documents\Embarcadero\Studio\Projects\NTP projekt\' +
+      'companycars.xml'
+    Left = 40
+    Top = 32
+  end
 end
--- Data/AllData.h
+++ Data/AllData.h
@@ -9,16 +9,22 @@
 #include "IniSettingsHelper.h"
 #include "ThemeHelper.h"
 #include "WRSettingsHelper.h"
+#include "Helpers/companycars.h"  //include for handling companycars.xml
+#include <Xml.XMLDoc.hpp>
+#include <Xml.xmldom.hpp>
+#include <Xml.XMLIntf.hpp>
 //---------------------------------------------------------------------------
 class TDataModule1 : public TDataModule
 {
 __published:	// IDE-managed Components
+	TXMLDocument *XmlDoc;
 private:	// User declarations
 public:		// User declarations
 	__fastcall TDataModule1(TComponent* Owner);
 	ThemeHelper themeHelper;
 	FontHelper fontHelper;
-    WRSettingsHelper wrSettingsHelper;
+	WRSettingsHelper wrSettingsHelper;
+	_di_IXMLcompanycarsType companycars = Getcompanycars(XmlDoc);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TDataModule1 *DataModule1;
