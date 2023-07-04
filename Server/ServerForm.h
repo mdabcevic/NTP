0d8e1857122d1f25dd3b895a411666ad5baa0e8e
maja <maja.dabcevic@gmail.com>
2023-07-04 20:29:51 +0200

    added a server with tcp client

---
+++ Server/ServerForm.h
@@ -0,0 +1,32 @@
+//---------------------------------------------------------------------------
+
+#ifndef ServerFormH
+#define ServerFormH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <IdBaseComponent.hpp>
+#include <IdComponent.hpp>
+#include <IdCustomTCPServer.hpp>
+#include <IdTCPServer.hpp>
+#include <IdContext.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TIdTCPServer *TCPServer;
+	void __fastcall TCPServerExecute(TIdContext *AContext);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+
+	// server actions
+    String FindAction(String code, TIdContext *AContext);
+	String AddToXml(TIdContext *AContext);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
